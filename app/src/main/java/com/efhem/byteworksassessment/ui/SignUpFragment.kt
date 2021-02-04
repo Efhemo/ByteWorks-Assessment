9469ebaacf83ee08888634a1caccdbfc8fce5ff6
Adegbite Femi <adegbitefemisamson@gmail.com>
2021-02-04 01:04:59 +0100

    collect and validate forms in sign in add employee fragment

--- app/src/main/java/com/efhem/byteworksassessment/ui/SignUpFragment.kt
+++ app/src/main/java/com/efhem/byteworksassessment/ui/SignUpFragment.kt
@@ -1,16 +1,25 @@
 package com.efhem.byteworksassessment.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.efhem.byteworksassessment.R
 import com.efhem.byteworksassessment.databinding.FragmentSignUpBinding
 import com.efhem.byteworksassessment.viewmodels.SignUpViewModel
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 class SignUpFragment : Fragment() {
 
@@ -19,6 +28,12 @@ class SignUpFragment : Fragment() {
     private lateinit var navController: NavController
 
     private val viewModel: SignUpViewModel by viewModels()
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        if (uri != null) {
+            startCropImage(uri)
+        }else Toast.makeText(requireContext(), getString(R.string.photo_not_selected), Toast.LENGTH_LONG).show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +45,7 @@ class SignUpFragment : Fragment() {
         bind.viewmodel = viewModel
         bind.lifecycleOwner = this
 
+        bind.photoContainer.setOnClickListener { selectImage() }
         bind.gender.setOnCheckedChangeListener { _, checkedId ->
             if(checkedId == R.id.male){
                 viewModel.setGender("Male")
@@ -52,6 +68,27 @@ class SignUpFragment : Fragment() {
                 }
             }
         })
+    }
+
+    private fun selectImage() {
+        getContent.launch("image/*")
+    }
+
+    private fun startCropImage(uri: Uri) {
+        CropImage.activity(uri)
+            .setGuidelines(CropImageView.Guidelines.ON)
+            .setAspectRatio(1920, 2480)
+            .setCropShape(CropImageView.CropShape.RECTANGLE)
+            .start(requireContext(), this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val result = CropImage.getActivityResult(data)
+            viewModel.setPhoto(result.uri.toString())
+            Glide.with(requireContext()).load(result.uri).apply(RequestOptions.fitCenterTransform()).into(bind.imgPhoto)
+        }
     }
 
     override fun onDestroyView() {
